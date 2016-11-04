c7123e46338936ebe760b5729fbf65fdf1c7c2d6
lucasbrambrink <lbrambrink@gmail.com>
2016-11-03 20:26:12 -0400

    adds async library

---
+++ load-async.js
@@ -0,0 +1,99 @@
+/*  Maximize concurrent javascript loading
+*   by loading all javascript asynchronously
+*   but maintain dependency relationships
+*
+*   <style>
+*       .async-script {
+*           display: none;
+*       }
+*   </style>
+*   <div class="async-script"
+*        data-key="baseLibrary"
+*        data-source="/scripts/useful/library.js">
+*   </div>
+*   <div class="async-script"
+*        data-key="specificJavascript"
+*        data-source="/scripts/pages/ui-components.js"
+*        data-depends-on="baseLibrary">
+*   <div>
+*
+* */
+
+var LoadAsync = (function(window) {
+
+    var broadcastReady = function(namespace) {
+        var event = new CustomEvent(namespace);
+        window.dispatchEvent(event);
+    };
+
+    var getScriptData = function(element) {
+        var data = {
+            key: element.getAttribute("data-key"),
+            source: element.getAttribute("data-source"),
+            dependsOn: element.getAttribute("data-depends-on")
+        };
+        data.dispatchReady = function() {
+            broadcastReady(data.key);
+        };
+        data.loadScriptCallback = function() {
+            var script = createScript(data);
+            document.body.appendChild(script);
+        };
+        return data;
+    };
+
+    var createScriptElement = function(data) {
+        var script = document.createElement("script");
+        script.src = data.source;
+
+        if (script.readyState){ //IE
+            script.onreadystatechange = function(){
+                if (script.readyState === "loaded" ||
+                        script.readyState === "complete"){
+                    script.onreadystatechange = null;
+                    data.dispatchReady();
+                }
+            };
+        } else {  // Other browsers
+            script.onload = function() {
+                data.dispatchReady();
+            };
+        }
+        return script;
+    };
+
+    var addDeferredScript = function(scriptElement) {
+        if (scriptElement === undefined || scriptElement === null) return;
+        var data = getScriptData(scriptElement);
+        if (!data.key && !data.source) return;
+
+        if (data.dependsOn === undefined || data.dependsOn === "") {
+            var script = createScriptElement(data);
+            document.body.appendChild(script);
+        } else {
+            window.addEventListener(data.dependsOn, data.loadScriptCallback);
+        }
+    };
+
+    var addScripts = function() {
+        var asyncScripts = document.getElementsByClassName('async-script');
+        for (var i = 0; i < asyncScripts.length; i++) {
+            addDeferredScript(asyncScripts[i]);
+        }
+        for (var j = 0; j < asyncScripts.length; j++) {
+            asyncScripts[j].remove();
+        }
+    };
+
+    return {
+        addScripts: addScripts
+    }
+})(window);
+
+if (window.addEventListener) {
+    window.addEventListener("load", LoadAsync.addScripts, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", LoadAsync.addScripts);
+} else {
+    window.onload = LoadAsync.addScripts;
+}
